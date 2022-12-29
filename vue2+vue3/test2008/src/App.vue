d68bb2931575ddeeec0ebb15dee3f021a443bfdf
lichenchen <574306720@qq.com>
2022-12-29 17:50:27 +0800

    12/29/2022

---
+++ vue2+vue3/test2008/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    hello app-{{myname}}
+    <input type = "text" v-model="mytext">
+    <button @click="handleAdd()">add</button>
+    <ul>
+      <li v-for="data in datalist" :key="data">
+      {{data}}</li>
+    </ul>
+    <navbar></navbar>
+  </div>
+</template>
+
+<script>
+import navbar from './components/Navbar'
+import Vue from 'vue'
+Vue.component('navbar',navbar)
+// ES6导出规范
+export default{
+  data(){
+    return{
+      myname:"LCC",
+      mytext:"",
+      datalist:[]
+    }
+  },methods:{
+    handleAdd(){
+      console.log(this.mytext)
+      this.datalist.push(this.mytext)
+    }
+  },
+  computed:{
+
+  },
+  watch:{
+
+  }
+}
+
+</script>
+
+<style lang="scss">
+ $w:300px;
+ul li{
+  background:yellow;
+  width: $w;
+}
+</style>
---
+++ vue2+vue3/test2008/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'// ES6 导入方式
+import App from './App.vue' // 导入根组件app
+// import router from './router'
+// import store from './store'
+
+Vue.config.productionTip = false
+
+new Vue({
+  // router,
+  // store,
+  render: h => h(App)
+}).$mount('#app')
+
+var obj = {
+  name: 'LCC',
+  age: 100
+}
+console.log(obj)
